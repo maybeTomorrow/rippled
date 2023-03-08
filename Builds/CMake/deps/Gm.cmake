63ca0c91b0c41f380c9de207f17d62b0c4928df0
maybeTomorrow <lin.xinpu@gmail.com>
2023-03-08 14:13:27 +0800

    gm fix

--- Builds/CMake/deps/Gm.cmake
+++ Builds/CMake/deps/Gm.cmake
@@ -4,11 +4,6 @@ add_subdirectory(src/gm)
 
 add_library(SmCrypto::gm ALIAS gm)
 
-target_link_libraries(ripple_libs INTERFACE SmCrypto::gm)
-
-
-
-
 find_package(GMP REQUIRED)
 
 include_directories(${GMP_INCLUDE_DIR})
@@ -16,6 +11,8 @@ link_directories(${GMP_LIBRARY_DIR})
 
 
 target_link_libraries(gm INTERFACE gmp gmpxx)
+
+target_link_libraries(ripple_libs INTERFACE SmCrypto::gm)
 
 
 install(FILES src/gm/gm.h DESTINATION include/gm)
