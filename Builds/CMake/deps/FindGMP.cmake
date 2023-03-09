8b35d7a8f582d14fb3c059d0020815f23b006cf1
maybeTomorrow <lin.xinpu@gmail.com>
2023-03-09 10:49:48 +0800

    fix gmp

--- Builds/CMake/deps/FindGMP.cmake
+++ Builds/CMake/deps/FindGMP.cmake
@@ -1,12 +1,20 @@
 set(GMP_PREFIX "" CACHE PATH "path ")
 
  
-find_path(GMP_INCLUDE_DIR gmp.h gmpxx.h PATHS ${GMP_PREFIX}/lib /usr/include /usr/local/include)
-find_library(GMP_LIBRARY  gmp gmpxx PATHS ${GMP_PREFIX}/lib /usr/lib /usr/local/lib)
+find_path(GMP_INCLUDE_DIR gmp.h PATHS ${GMP_PREFIX}/lib /usr/include /usr/local/include)
+ 
+
+if(static)
+  set(GMP_LIB libgmp.a)
+else()
+  set(GMP_LIB gmp)
+endif()
+
+find_library(GMP_LIBRARY NAMES ${GMP_LIB} PATHS ${GMP_PREFIX}/lib /usr/lib /usr/local/lib)
 
 
 if(GMP_INCLUDE_DIR AND GMP_LIBRARY)
-    get_filename_component(GMP_LIBRARY_DIR ${GMP_LIBRARY} PATH)
+    # get_filename_component(GMP_LIBRARY_DIR ${GMP_LIBRARY} PATH)
     set(GMP_FOUND TRUE)
 endif()
 
--- Builds/CMake/deps/Gm.cmake
+++ Builds/CMake/deps/Gm.cmake
@@ -7,12 +7,9 @@ add_library(SmCrypto::gm ALIAS gm)
 find_package(GMP REQUIRED)
 
 include_directories(${GMP_INCLUDE_DIR})
-link_directories(${GMP_LIBRARY_DIR})
 
-
-target_link_libraries(gm INTERFACE gmp gmpxx)
+target_link_libraries(gm INTERFACE gmp)
 
 target_link_libraries(ripple_libs INTERFACE SmCrypto::gm)
-
 
 install(FILES src/gm/gm.h DESTINATION include/gm)
